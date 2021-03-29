60242b4bf71acf79c8b26064305c11d4dfe760c1
Kevin Silvestre <kevsilvo@gmail.com>
2021-03-29 15:01:17 -0400

    dropdown desktop functional

--- js/index.js
+++ js/index.js
@@ -7,12 +7,27 @@ function mobileMenu() {
     }
 }
 
-function closeOpen(id) {
+function closeOpen(id, selector, medium) {
     //close other dropdowns if open, so there is only one at a time showing
-    var toClose = document.querySelectorAll(".dropdown_mobile");
-    for (let i = 0; i < toClose.length; i++) {
-        if (toClose[i].id != id) {
-            toClose[i].style.display = "none"
+    var toClose = document.querySelectorAll(selector);
+    if (medium === "mobile") {
+        for (let i = 0; i < toClose.length; i++) {
+            if (toClose[i].id != id) {
+                toClose[i].style.display = "none"
+            }
+
+
+        }
+    }
+    if (medium === "desktop") {
+        for (let i = 0; i < toClose.length; i++) {
+            var toCheck = document.querySelectorAll(selector + "+" + ".nested_ul");
+            if (toClose[i].id != id && toCheck[i].classList.contains("dropdown_hover")) {
+                toCheck[i].classList.remove("dropdown_hover");
+
+            }
+
+
         }
 
 
@@ -22,16 +37,25 @@ function closeOpen(id) {
 
 }
 
-function changeIcon(id) {
-    var icon = document.querySelector(".dropdown_title_mobile#" + id + " > #change");
+function changeIcon(id, medium) {
+    var icon;
+    if (medium === "mobile") {
+        icon = document.querySelector(".dropdown_title_mobile#" + id + " > #change");
+
+    }
+    if (medium === "desktop") {
+        icon = document.querySelector(".dropdown_title#" + id + " > #change");
+
+    }
+
     icon.classList.toggle("rotate_arrow");
 
 }
 
 function dropdownMenu(id) {
     var toDisplay = document.querySelector(".dropdown_mobile#" + id);
-    closeOpen(id);
-    changeIcon(id);
+    closeOpen(id, ".dropdown_mobile", "mobile");
+    changeIcon(id, "mobile");
     if (toDisplay.style.display === "block") {
         toDisplay.style.display = "none";
     } else {
@@ -42,7 +66,9 @@ function dropdownMenu(id) {
 }
 
 var showDropdown = function (e) {
-    const toShow = document.querySelector(".dropdown_title + .nested_ul");
+    const toShow = document.querySelector("#" + e.id + "+" + ".nested_ul");
     toShow.classList.toggle("dropdown_hover");
-    console.log(toShow);
+    closeOpen(e.id, ".dropdown_title", "desktop");
+    changeIcon(e.id, "desktop");
+
 }
